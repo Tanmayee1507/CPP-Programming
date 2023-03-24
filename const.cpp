d5ef64748711411285952aa802dd301635bac3fc
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-03-24 10:58:01 +0530

    Create const.cpp
    concept of constant in cpp

---
+++ const.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+const int A = 10;
+
+int main()
+{
+    const int B = 20;
+    const int Arr[4] = {10, 20, 30, 40};
+
+    const int No = 11;
+    
+    return 0;
+
+}
