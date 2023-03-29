0e264f80ea198584d112dd884db28b28c92596f2
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-03-29 22:52:22 +0530

    Add files via upload

---
+++ stack.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int Addition(int No1, int No2)
+{
+    int Ans = 0;
+    Ans = No1 + No2;
+    return Ans;
+}
+
+int main()
+{
+    int A = 10, B = 20;
+    int iRet = 0;
+    
+    Ret = Addition(A, B);
+
+    cout<<"Addition is:"<<iRet<<"\n";
+
+    return 0;
+}
