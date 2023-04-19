83bd193499fcbc5cd1cb9a43ae16988e8b624436
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-04-19 18:34:35 +0530

    Add files via upload

---
+++ program1.cpp
@@ -0,0 +1,15 @@
+/*
+Function name: Jay GAnesh
+Author: Tanmayee Kadam
+Date:18th April 2023
+language :C++ programming
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Jay Ganesh"<<"\n";
+
+    return 0;
+}
