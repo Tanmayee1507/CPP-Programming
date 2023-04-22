658312e56be203ddc52f430f6260aebb19b381ae
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-04-23 00:20:43 +0530

    Add files via upload

---
+++ program2.cpp
@@ -0,0 +1,21 @@
+/*
+Function name: Addition
+Author: Tanmayee Kadam
+Date:18th April 2023
+language :C++ programming
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int No1 = 10;
+    int No2 = 11;
+    int Ans = 0;
+
+    Ans = No1 + No2;
+
+    cout<<"Addition is:"<<Ans<<"\n";
+
+}
