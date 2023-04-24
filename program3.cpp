cf5dcfb707aed9c931deb07363a0d3907946e7c5
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-04-24 23:21:33 +0530

    Add files via upload

---
+++ program3.cpp
@@ -0,0 +1,25 @@
+/*
+Function name: Addition while user gives the input
+Author: Tanmayee Kadam
+Date:18th April 2023
+language :C++ programming
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int No1 = 0;
+    int No2 = 0;
+    int Ans = 0;
+
+    cout<<"Enter first number:"<<"\n";
+    cin>>No1;
+    cout<<"Enter second number:"<<"\n";
+    cin>>No2;
+
+    Ans = No1 + No2;
+
+    cout<<"Addition is:"<<Ans<<"\n";
+}
