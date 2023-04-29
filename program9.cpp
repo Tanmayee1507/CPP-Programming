efa026d02af9a1cbcd5b38e5e5e5ca34579fe48f
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-04-30 00:50:27 +0530

    Add files via upload

---
+++ program9.cpp
@@ -0,0 +1,22 @@
+/*
+Function Name:Sequence
+output: Jay ganesh 5 times
+Description: Display Jay Ganesh 5 times
+Author: Tanmayee K Deepak
+Date:  26 April 2023
+Language:CPP Programming
+*/
+
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    cout<<"Jay Ganesh...\n";
+    cout<<"Jay Ganesh...\n";
+    cout<<"Jay Ganesh...\n";
+    cout<<"Jay Ganesh...\n";
+    cout<<"Jay Ganesh...\n";
+
+    return 0;
+}
