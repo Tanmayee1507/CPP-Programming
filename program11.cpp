32fcb9024e6b265dbafa33977bb3d478466db2e7
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-05-01 00:16:43 +0530

    Add files via upload

---
+++ program11.cpp
@@ -0,0 +1,22 @@
+/*
+Function Name:for loop in main
+output: Jay ganesh 5 times
+Description: Display Jay Ganesh 5 times
+Author: Tanmayee K Deepak
+Date:  26 April 2023
+Language:CPP Programming
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int iNo = 5;
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt <= iNo; iCnt ++)
+    {
+        cout<<"Jay Ganesh...\n";
+    }
+    return 0;
+}
