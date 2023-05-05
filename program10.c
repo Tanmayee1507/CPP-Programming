58ef98117e5b4979494f90bf8721014a5f0d89c3
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-05-05 22:54:12 +0530

    Add files via upload

---
+++ program10.c
@@ -0,0 +1,13 @@
+//Dizplay Jay GAnesh using iteration
+#include<stdio.h>
+
+int main()
+{
+    int iCnt = 0;
+            //1     //3         //3
+    for(iCnt = 1; iCnt <= 5; iCnt ++)
+    {
+        printf("Jay Ganesh...\n"); //4
+    }
+    return 0;
+}
