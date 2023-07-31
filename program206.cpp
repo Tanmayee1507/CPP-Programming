b8aab3aa7f8c5c70f01c08774060bcf690ae44fc
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-07-31 17:36:33 +0530

    Add files via upload

---
+++ program206.cpp
@@ -0,0 +1,40 @@
+//using class and characteristics
+#include<iostream>
+using namespace std;
+
+class Number
+{
+    public:
+    int iNo1;// characteristics
+    int iNo2;
+        int Maximum()
+        {
+            if(iNo1>iNo2)
+            {
+                return iNo1;
+            }
+            else
+            {
+                return iNo2;
+            }
+        }
+};
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+    int iRet= 0;
+    Number nobj;
+   
+    cout<<"Enter first number:"<<"\n";
+    cin>>iValue1;
+     nobj.iNo1 = iValue1;
+    cout<<"Enter second number:"<<"\n";
+    cin>>iValue2;
+    nobj.iNo2 = iValue2;
+
+    iRet = nobj.Maximum();
+    cout<<"Maximum number is:"<<iRet<<"\n";
+return 0;
+}
