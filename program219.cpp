bc1ac9972ddce5f5824ddbffb1f574cf93ab1ea3
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-08-05 21:32:22 +0530

    Add files via upload

---
+++ program219.cpp
@@ -0,0 +1,43 @@
+/*problem on digits
+using temp variable
+*/
+#include<iostream>
+using namespace std;
+
+class Digit
+{
+    public:
+    int iNo;
+
+    Digit(int X)
+    {
+        iNo = X;
+    }
+    int SumDigits()
+    {
+        int iDigit = 0;
+        int iSum = 0;
+       
+        while(iNo != 0)
+        {
+            iDigit = iNo % 10;
+            iSum = iSum + iDigit;
+            iNo = iNo / 10;
+        }
+        return iSum;
+    }
+};
+
+int main()  //1
+{
+    int iValue = 0;
+    int iRet = 0;
+    cout<<"Enter the number:"<<"\n";
+    cin>>iValue;
+
+    Digit dobj(iValue);
+    iRet = dobj.SumDigits();
+    cout<<"Summation of Digits:"<<iRet<<"\n";
+
+    return 0;
+}
