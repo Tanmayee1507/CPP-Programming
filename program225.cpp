dae7f64bbccedaddf635e3e1329674285b4a1feb
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-08-07 14:36:51 +0530

    Add files via upload

---
+++ program225.cpp
@@ -0,0 +1,29 @@
+//take decimal number from user and convert it into binary format
+#include<iostream>
+using namespace std;
+
+void DisplayBinary(int iNo)
+{
+    int iDigit = 0;
+
+    while(iNo != 0)
+    {
+        iDigit = iNo%2;
+        cout<<iDigit;
+        iNo = iNo/2;
+
+    }
+    cout<<"\n";
+
+}
+
+int main()
+{
+    int iValue = 0;
+
+    cout<<"Enter the decimal number"<<"\n";
+    cin>>iValue;
+
+    DisplayBinary(iValue);
+    return 0;
+}
