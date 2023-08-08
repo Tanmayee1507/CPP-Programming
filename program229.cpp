4442c5bbcbf0e114a826a88144596efbd38cbdbc
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-08-08 22:52:32 +0530

    Add files via upload

---
+++ program229.cpp
@@ -0,0 +1,57 @@
+//take decimal number from user and convert it into hexadecimal format
+#include<iostream>
+using namespace std;
+
+void DisplayHexadecimal(int iNo)
+{
+    int iDigit = 0;
+    
+    cout<<"Hexadecimal conversion of:"<<iNo <<" is:"<<"\n";
+    while(iNo != 0)
+    {
+        iDigit = iNo%16;
+        if(iDigit<= 9)
+        {
+        cout<<iDigit;
+        }
+        else
+        {
+            switch(iDigit)
+            {
+                case 10:
+                cout<<"A";
+                break;
+                case 11:
+                cout<<"B";
+                break;
+                case 12:
+                cout<<"C";
+                break;
+                case 13:
+                cout<<"D";
+                break;
+                case 14:
+                cout<<"E";
+                break;
+                case 16:
+                cout<<"F";
+                break;
+            }
+        }
+        iNo = iNo/16;
+
+    }
+    cout<<"\n";
+
+}
+
+int main()
+{
+    int iValue = 0;
+
+    cout<<"Enter the decimal number"<<"\n";
+    cin>>iValue;
+
+    DisplayHexadecimal(iValue);
+    return 0;
+}
