3d1a6781dd58d5daef80ea3bc37dc8b20b2230c7
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-08-08 22:51:49 +0530

    Add files via upload

---
+++ program226.cpp
@@ -0,0 +1,29 @@
+//take decimal number from user and add binary value
+#include<iostream>
+using namespace std;
+
+void DisplayBinaryResults(int iNo1, int iNo2)
+{   
+    int iResult = 0;
+    iResult = iNo1 & iNo2;
+    cout<<"Result of & is:"<<iResult<<"\n";
+    iResult = iNo1 | iNo2;
+    cout<<"Result of | is:"<<iResult<<"\n";
+    iResult = iNo1 ^ iNo2;
+    cout<<"Result of ^ is:"<<iResult<<"\n";
+
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    cout<<"Enter the first number"<<"\n";
+    cin>>iValue1;
+
+cout<<"Enter the second number"<<"\n";
+    cin>>iValue2;
+    DisplayBinaryResults(iValue1, iValue2);
+    return 0;
+}
