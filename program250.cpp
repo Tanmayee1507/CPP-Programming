a4295fac3653cef32d6a22754747245adda2c393
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-08-15 22:44:46 +0530

    Add files via upload

---
+++ program250.cpp
@@ -0,0 +1,31 @@
+//accept a no and bit position from user and off that bit
+
+#include<iostream>
+
+using namespace std;
+
+typedef unsigned int UINT;
+
+UINT OffBit(UINT iNo, UINT iPos)
+{
+    UINT iMask = 0XFFFFFFBF;
+    
+         iNo & iMask;
+
+         return (iNo & iMask) ;
+       
+}
+
+int main()
+{
+    UINT iValue = 0;
+    UINT iBit = 0;
+    UINT iRet = 0;
+    cout<<"Enter the decimal number"<<"\n";
+    cin>>iValue;
+    cout<<"Enter the bit position"<<"\n";
+    cin>>iBit;
+    iRet = OffBit(iValue, iBit);
+  cout<<"Result is:"<<iRet<<"\n";
+    return 0;
+}
