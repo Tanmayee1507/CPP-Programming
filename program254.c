b8a539e4f668a6e79d828629280ec301254164bd
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-08-16 23:22:16 +0530

    Add files via upload

---
+++ program254.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int iNo;
+    struct Node *next;
+};
+
+typedef struct Node Node;
+typedef struct Node *PNODE;
+typedef struct Node **PPNODE; 
+
+void Display(PNODE Head)
+{
+
+}
+int Count(PNODE Head)
+{
+    return 0;
+}
+void InserFisrt(PPNODE Head, int No)
+{
+
+}
+void InsertLast(PPNODE Head, int No)
+{
+
+}
+void InsertAtPos(PPNODE Head, int No, int Pos)
+{
+
+}
+void DeleteFirst(PPNODE Head)
+{
+
+}
+void DeleteLast(PPNODE Head)
+{
+
+}
+void DeleteAtPos(PPNODE Head, int Pos)
+{
+
+}
+int main()
+{
+    PNODE First = NULL;
+    return 0;
+}
