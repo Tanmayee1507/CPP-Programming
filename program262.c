ddb5121da330ed9b7ca3ecbf12f846a4f2f4dabc
Tanmayee Kadam <86916999+Tanmayee1507@users.noreply.github.com>
2023-08-19 01:38:50 +0530

    Add files via upload

---
+++ program262.c
@@ -0,0 +1,80 @@
+//Doubly Linked List
+#include<stdio.h>
+#include<stdlib.h>
+//program271 insert first
+//# added in new line addes
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+    struct Node *prev;  //#
+}NODE, *PNODE, **PPNODE;
+
+void InsertFirst(PPNODE Head, int No)// head call by address
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+    newn->data = No;
+    newn->next = NULL;
+    newn->prev = NULL;   //#
+
+    if(*Head ==NULL)// LL is empty
+    {
+        *Head = newn;
+    }
+    else//LL contains atleast 1 node
+    {
+        (*Head) ->prev = newn;
+        newn->next = *Head;
+        *Head = newn;
+    }
+}
+void InsertLast(PPNODE Head, int No)
+{
+}
+void InsertAtPos(PPNODE Head, int No, int iPos)
+{}
+void DeleteFirst(PPNODE Head)
+{}
+void DeleteLast(PPNODE Head)
+{}
+void DeleteAtPos(PPNODE Head, int iPos)
+{}
+void Display(PNODE Head)
+{
+    printf("Elements of Linked List are: \n");
+    printf("NULL<=>");
+    while(Head != NULL)
+    {
+        printf("|%d|<=>",Head->data);
+        Head= Head-> next;
+    }
+    printf("NULL \n");
+}
+int Count(PNODE Head)
+{
+    int iCount = 0;
+    
+    while(Head != NULL)
+    {
+       iCount++;
+        Head= Head-> next;
+    }
+   
+    return iCount;
+}
+
+int main()
+{
+    PNODE First = NULL;
+    int iRet = 0;
+    InsertFirst(&First, 101);
+    InsertFirst(&First, 51);
+    InsertFirst(&First, 21);
+    InsertFirst(&First, 11);
+    iRet = Count(First);
+    printf("No of elements are:%d\n", iRet);
+    Display(First);
+    return 0;
+}
